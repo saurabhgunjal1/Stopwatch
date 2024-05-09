97beb452e87a7a11e7bdcab5b57ac6a76ba4dff3
Saurabh Gunjal <gunjalsaurabh16@gmail.com>
2024-05-09 17:48:21 +0530

    stopwatch done

---
+++ src/stopwatch.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import "./stopwatch.css"
+export default function Stopwatch() {
+  const [isRunning, setisRunning] = useState(false);
+  const [time, setTime] = useState(0);
+useEffect(()=>{
+    let timer;
+    if(isRunning){
+        timer = setInterval(()=>{
+            setTime(prevTime => prevTime+1);
+        }, 1000);
+    }else{
+        clearInterval(timer);
+    }
+    return ()=> clearInterval(timer);
+}, [isRunning]);
+
+  const handlestartstop = ()=>{
+        setisRunning(prevIsRunning => !prevIsRunning);
+  };
+
+  const handleReset = ()=>{
+    
+    setTime(0);
+    setisRunning(false);
+  }
+
+
+  const formatTime = (seconds) => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes}:${remainingSeconds < 10 ? "0" : ""}${remainingSeconds}`;
+  };
+  return (
+    <div>
+      <h1>Stopwatch</h1>
+      <p>{formatTime(time)}</p>
+      <div className="controls">
+        {isRunning ? (
+          <button onClick={handlestartstop} className="btn">Stop</button>
+        ) : (
+          <button onClick={handlestartstop} className="btn">Start</button>
+        )}
+          <button onClick={handleReset} className="btn">Reset</button>
+
+      </div>
+    </div>
+  );
+}
